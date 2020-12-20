aa3de48c5aec25418ce1e83a90f804db1599d88d
nberger62 <nathanberger62@gmail.com>
2020-12-20 07:59:55 -0500

    creating a mern app for future site. set up backend, database, and react create react app client.

---
+++ server/db/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+mongoose
+    .connect('mongodb://127.0.0.1:27017/cinema', { useNewUrlParser: true })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+
+module.exports = mongoose.connection
